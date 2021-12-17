2b9338f9a08840dde04ad50ff91f864695193da0
jmihelson <90315813+jmihelson@users.noreply.github.com>
2021-12-17 16:04:08 +0200

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,1 +1,29 @@
 
+
+// Kodutöö
+// 0-20 liiga külm      -1
+// 21-40 paras           0
+// 40-60 liiga kuum     1
+// checkTemp(temp)
+
+// Faili nimi checkTemp.js
+// Githubi ülesse
+
+
+
+function checkTemp(a){
+    if (a<21){
+        console.log("liiga külm")
+    return -1
+}
+    else if (a>40){
+        console.log("liiga kuum")
+    return 0
+    }
+    else{
+        console.log("paras")
+    return 0
+    }
+}
+
+checkTemp(30)
